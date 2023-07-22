f844bd0a2e60347a4e9fd885e4e3fd5395045302
vedant jangid <vedant@vedants-MacBook-Air.local>
2023-07-22 23:53:33 +0530

    commit all

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ export default function App(props) {
   return (
     <>
       <Head>
-        <title>MEOWU</title>
+        <title>WOFFLETOPIA</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
